ab3f3ffa824d4cf4186d5fab8cbf7684cf848d6a
hyunmun <2366420848@qq.com>
2019-08-30 17:21:23 +0800

    实验1

--- 1806101074赵贤雯JAVA/src/lab01/Test_05.java
+++ 1806101074赵贤雯JAVA/src/lab01/Test_05.java
@@ -3,10 +3,10 @@ package lab01;
 public class Test_05 {
     public static void main(String[] args) {
         int score = 3;
-        String str="不及格";
+        String str = "不及格";
         if (score == 1) {
             str = "不及格";
-        }else if (score == 2) {
+        } else if (score == 2) {
             str = "及格";
         } else if (score == 3) {
             str = "中等";
@@ -18,3 +18,4 @@ public class Test_05 {
         System.out.println(str);
     }
 }
+
