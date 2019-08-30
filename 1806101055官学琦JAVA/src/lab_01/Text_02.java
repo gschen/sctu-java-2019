bb1811fe8eac1d358fbf98e6edb00b7db8f1b986
guanxueqi <1529322913@qq.com>
2019-08-30 14:19:31 +0800

    实验一，使用continue语句实现：将100-300之间的不能被3整除的数输出。

---
+++ 1806101055官学琦JAVA/src/lab_01/Text_02.java
@@ -0,0 +1,12 @@
+package lab_01;
+
+public class Text_02 {
+    public static void main(String[] args) {
+        for (int i = 100; i <=300; i++) {
+            if (i%3!=0){
+                continue;
+            }
+            System.out.print(i);
+        }
+    }
+}
