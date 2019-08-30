12db43a3cb20e3d60b2171dcd152c134803cbba8
xiaofwnghuang <hxf08520215>
2019-08-30 14:10:20 +0800

    。

---
+++ 18061101025黄晓锋/src/day20190829/Test2_1.java
@@ -0,0 +1,14 @@
+package day20190829;
+
+public class Test2_1 {
+    public static void main(String[] args) {
+        //程序名称：Je020201
+        //功能:演示字符与unicode字符表编码的关系
+        public class Je020201{
+            public static void main(String[] args) {
+                final char SQ=39;   //单引号： single quote
+                char
+            }
+        }
+    }
+}
