a946e201930b0d8dfdcf75245984d7c1b543463a
guanxueqi <1529322913@qq.com>
2019-08-30 14:54:38 +0800

    实验一，4、	分别用 for 语句、while 语句以及 do while 语句编写一个求和程序（即 sum=1+2+3+...+n）。

---
+++ 1806101055官学琦JAVA/src/lab_01/Test_05.java
@@ -0,0 +1,50 @@
+package lab_01;
+
+import java.util.Scanner;
+
+public class Test_05 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("请输入数值：");
+        int x=input.nextInt();
+        if (x==1){
+            System.out.println("不及格");
+        }
+        else if (x==2){
+            System.out.println("及格");
+        }
+        else if (x==3){
+            System.out.println("中等");
+        }
+        else if (x==4){
+            System.out.println("良好");
+        }
+        else if (x==5){
+            System.out.println("优秀");
+        }
+        else {
+            System.out.println("您输入的值的范围不在1-5之间（包含1和5），请重新运行程序");
+        }
+        //第二种方法
+        switch (x){
+            case 1:
+                System.out.println("不及格");
+                break;
+            case 2:
+                System.out.println("及格");
+                break;
+            case 3:
+                System.out.println("中等");
+                break;
+            case 4:
+                System.out.println("良好");
+                break;
+            case 5:
+                System.out.println("优秀");
+                break;
+            default:
+                System.out.println("您输入的值的范围不在1-5之间（包含1和5），请重新运行程序");
+                break;
+        }
+    }
+}
