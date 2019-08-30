b746382ca24d35caaf2a8c3d9af8d384a24deb6a
pony <1176632450@qq.com>
2019-08-30 16:07:04 +0800

    实验1到7

---
+++ 1806101049马原涛JAVA/src/lab_01/Test_04_1.java
@@ -0,0 +1,11 @@
+package lab_01;
+
+public class Test_04_1 {public static void main(String[] a)
+    {int sum,n;
+    sum=0;n=100;
+        for (int i=1;i<=n;i++){
+            sum=sum+i;
+        }
+        System.out.println(sum);
+    }
+}
