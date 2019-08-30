dfc53b2af69e9a0602540a34100bb5e8bf8d8cda
chengcaijason <2608831274@qq.com>
2019-08-30 15:58:59 +0800

    练习

---
+++ 1806101098杨金月JAVA/src/lab01/text_04.java
@@ -0,0 +1,31 @@
+package lab01;
+import java.util.Scanner;
+import java.util.concurrent.SynchronousQueue;
+
+public class text_04 {
+    public static void main(String args[]) {
+        Scanner input=new Scanner(System,in)
+        int sum=0;
+
+        //for
+        for(int i=1;i<=n;i++){
+            sum+=i;}
+        System.out.println(sum);
+       /* //while
+        int i=1;
+        sum=0;
+        while(i<=n){
+            sum+=i;
+            i++;
+        }
+        System.out.println(sum);
+        //do-while
+        i=1;
+        sum=0;
+        do{
+            sum+=i;
+            i++;
+        }while(i<=n);
+        System.out.println(sum);*/
+    }
+}
