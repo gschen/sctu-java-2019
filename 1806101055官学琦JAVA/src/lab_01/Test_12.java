9208279f2a8ff96381b8ba3b506659e2b87b0129
guanxueqi <1529322913@qq.com>
2019-08-31 14:42:55 +0800

    实验一，水仙花素判断

--- 1806101055官学琦JAVA/src/lab_01/Test_12.java
+++ 1806101055官学琦JAVA/src/lab_01/Test_12.java
@@ -8,7 +8,7 @@ public class Test_12 {
         //Scanner input=new Scanner(System.in);
         //System.out.print("请输入一个数字，判断是否为水仙花素：");
         //int x=input.nextInt();
-        for (int i = 1; i <=1000 ; i++) {
+        for (int i = 1; i <1000 ; i++) {
             boolean flag=true;
             if (Narcissus(i)==flag){
                 System.out.println(i+"是水仙花素");
