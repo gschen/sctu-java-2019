989ecf5db8f0465e6e065aa863e2b414b727c38e
wenkangxi <2441878030@qq.com>
2019-08-31 11:28:17 +0800

    lab1-4

--- 1806101076温康爔JAVA/src/lab_01/Test_04.java
+++ 1806101076温康爔JAVA/src/lab_01/Test_04.java
@@ -2,7 +2,33 @@ package lab_01;
 
 public class Test_04 {
     public static void main(String[] args) {
-        int a=0;
+        int n=Integer.parseInt(args[0]);
+        int sum=0;
 
+        //for循环
+        for (int i=0;i<=n;i++){
+            sum+=i;
+        }
+        System.out.println(sum);
+
+        //while
+        int i=1;
+        sum=0;
+        while(i<=n){
+            sum+=i;
+            i++;
+        }
+        System.out.println(sum);
+
+        //do-while
+        i=1;
+        sum=0;
+        do{
+            sum+=i;
+            i++;
+        }while(i<=n);
+        System.out.println(sum);
     }
 }
+
+
