ee1f999c6c4fe3637bf81d2ab17b68199849195a
guanxueqi <1529322913@qq.com>
2019-08-31 13:48:11 +0800

    实验一，素数的判断

---
+++ 1806101055官学琦JAVA/src/lab_01/Test_10.java
@@ -0,0 +1,22 @@
+package lab_01;
+
+import java.util.Scanner;
+
+public class Test_10 {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        int x=input.nextInt();
+        boolean flag=true;//是素数
+        for (int i = 2; i <x ; i++) {
+            if (x%i==0){
+                flag=false;//不是素数
+            }
+        }
+        if (flag){
+            System.out.println(x+"是素数");
+        }
+        else {
+            System.out.println(x+"不是素数");
+        }
+    }
+}
