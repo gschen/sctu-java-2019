28ed33be75526ed10c301b3b18c13d23c7c308ac
heyaoyy <1946550852@qq.com>
2019-09-01 11:18:35 +0800

    实验

--- 1806101068何瑶JAVA/src/lab_01/Test_08.java
+++ 1806101068何瑶JAVA/src/lab_01/Test_08.java
@@ -6,7 +6,7 @@ public class Test_08 {
         int col=0;
         for (row=1;row<=9;row++){
             for (col=1;col<=row;col++) {
-                System.out.print(col+"*"+row+"="+row*col);
+                System.out.print(col+"*"+row+"="+row*col+"");
             }
             System.out.println();
         }
