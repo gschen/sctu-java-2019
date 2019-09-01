0e3f3547e7c6eb0bbd4679e9e516625797b34546
yangxi0118 <1751194165@qq.com>
2019-09-01 17:31:05 +0800

    test6

--- 1806101016杨曦JAVA/src/lab01/test06.java
+++ 1806101016杨曦JAVA/src/lab01/test06.java
@@ -1,4 +1,24 @@
 package lab01;
 
+import java.util.Scanner;
 public class test06 {
-}
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("请输入n的值:");
+        int n=sc.nextInt();
+        if(n>=0&&n<=59){
+            System.out.println("D");
+        }
+        else if(n>=60&&n<=69){
+            System.out.println("C");
+        }
+        else if(n>=70&&n<=84){
+            System.out.println("B");
+        }
+        else if(n>=85){
+            System.out.println("A");
+        }
+        System.out.println();
+        }
+    }
+
