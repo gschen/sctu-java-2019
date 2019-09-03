75a2505dc3beeb540c05bf20eeac2a79e8ff0d5e
xiaofwnghuang <hxf08520215>
2019-09-03 19:28:59 +0800

    练习

---
+++ 18061101025黄晓锋JAVA/src/lab01/Test_09.java
@@ -0,0 +1,10 @@
+package lab01;
+
+public class Test_09 {
+    public static void main(String[] args) {
+        switch (0){
+            case 1:
+
+        }
+    }
+}
---
+++ 18061101025黄晓锋JAVA/src/lianxi/E_04.java
@@ -0,0 +1,8 @@
+package lianxi;
+
+public class E_04 {
+    public static void main(String[] args) {
+        int a=2,b=6;
+        System.out.println((a++)+(b++)+a*b);
+    }
+}
