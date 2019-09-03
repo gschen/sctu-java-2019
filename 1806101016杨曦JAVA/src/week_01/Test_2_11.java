55609d0003c82545ffed9f092826222555bb877a
yangxi0118 <1751194165@qq.com>
2019-09-03 22:05:35 +0800

    test_2_11.java

--- 1806101016杨曦JAVA/src/week_01/Test_2_11.java
+++ 1806101016杨曦JAVA/src/week_01/Test_2_11.java
@@ -2,17 +2,16 @@ package week_01;
 
 public class Test_2_11 {
     public static void main(String[] args) {
-        String str = "ABCDEFGHIJKLMNOPQRST";
-        int index = 0;
-        for (int i = 0; i < 8; i++) {
-            int sum = (int)Math.abs(i-3.5);
-            printTab(sum);
-            for (int j=0;j<4-sum;j++) {
-                System.out.print(str.charAt(index)+" ");
-                index += 1;
-            }
-            System.out.println();
-        }
+        String str = "A B C D E F G H I J K L M N O P Q R S T ";
+       int n =0;
+       for (int i = 0;i<8;i++){
+           int sum=(int)Math.abs(i-3.5);
+           printTab(sum);
+           for (int j=0;j<4-sum;j++){
+               System.out.print(str.charAt(n)+"");
+               n+=1;
+           }
+       }System.out.println();
     }
 
     private static void printTab(int sum) {
