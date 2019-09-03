b72e8cfbb3f35c7a2cc3478c9a4240a0e4028549
zhangyunying <1607884857@qq.com>
2019-09-03 20:52:57 +0800

    text_07

---
+++ 1806101010张耘荧Java/src/lab_01/Text_07.java
@@ -0,0 +1,34 @@
+package lab_01;
+
+import java.util.Scanner;
+
+public class Text_07 {
+    public static void main(String[] args) {
+        int n=(int)(Math.random()*100);
+        //接受从控制台的输入
+        Scanner input=new Scanner(System.in);
+
+
+
+        while (true){
+            System.out.println("请输入成绩结果：");
+            //接受对象n
+            int guess=input.nextInt();
+
+            if (guess !=n) {
+                if (guess > n) {
+
+                    System.out.println("猜大了");
+                } else if (guess < n) {
+
+                    System.out.println("猜小了");
+                }
+            }
+            else{
+
+                System.out.println("猜对了");
+                break;
+            }
+        }
+    }
+}
