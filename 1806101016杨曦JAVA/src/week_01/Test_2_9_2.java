ebc92babc328d167b8f0a65f357ca7fc6cc53849
yangxi0118 <1751194165@qq.com>
2019-09-03 21:10:35 +0800

    test5_1

---
+++ 1806101016杨曦JAVA/src/week_01/Test_2_9_2.java
@@ -0,0 +1,33 @@
+package week_01;
+import java.util.Scanner;
+public class Test_2_9_2 {
+    public static void main(String[] args) {
+        System.out.println("请输入分数：");
+        Scanner input = new Scanner(System.in);
+        int i = input.nextInt();
+        i = i / 5;
+        String str="";
+        switch (i) {
+
+            case 1:
+                str=("不及格");
+                break;
+            case 2:
+                str=("及格");
+                break;
+            case 3:
+                str=("中等");
+                break;
+            case 4:
+                str=("良好");
+                break;
+            case 5:
+                str=("优秀");
+                break;
+
+        }
+
+        System.out.println(str);
+    }
+}
+
