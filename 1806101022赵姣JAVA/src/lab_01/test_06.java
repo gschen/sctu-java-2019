437d96ebd78db92d51bc023903ba11a2e0ae35af
zhaojiao188 <932513952@qq.com>
2019-09-03 22:49:01 +0800

    实验6-12

---
+++ 1806101022赵姣JAVA/src/lab_01/test_06.java
@@ -0,0 +1,26 @@
+package lab_01;
+    import java.util.*;
+public class test_06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入学生成绩：");
+        int j = sc.nextInt();
+        String str="";
+        if (85<=j){
+            str = "A级";
+        }
+        else if (70<=j&&85>j){
+            str = "B级";
+        }
+        else if (60<=j&&70>j){
+            str = "C级";
+        }
+        else if(60<j){
+            str = "D级";
+        }
+        else{
+            str = "输入有误！";
+        }
+        System.out.println("学生成绩等级为："+ str);
+    }
+}
