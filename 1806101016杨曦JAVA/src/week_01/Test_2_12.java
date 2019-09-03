87e1e25886bc8967722433995c227656bc681c86
yangxi0118 <1751194165@qq.com>
2019-09-03 23:00:53 +0800

    test_2_12_1

---
+++ 1806101016杨曦JAVA/src/week_01/Test_2_12.java
@@ -0,0 +1,14 @@
+package week_01;
+import java.util.Scanner;
+public class Test_2_12 {
+    public static void main(String[] args) {
+        Scanner mas = new Scanner(System.in);
+        System.out.println("请输入一个整数n：");
+        int n = mas.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
