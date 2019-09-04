393f0911301dfb6527f24a1d957fe7ab9341f1b3
jiaxue522 <1697424698@qq.com>
2019-09-04 22:34:30 +0800

    test08

--- 1806101018贾雪Java/src/lab01/Test08.java
+++ 1806101018贾雪Java/src/lab01/Test08.java
@@ -1,6 +1,13 @@
 package lab01;
 
 public class Test08 {
-
+    public static void main(String[] args) {
+        for (int i = 1; i <=9 ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(j+"x"+i+"="+(j*i)+"\t");
+            }
+            System.out.println();
+        }
+    }
 
 }
