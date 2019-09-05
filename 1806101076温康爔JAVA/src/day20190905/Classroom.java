b2a89a6baaf17816ecd8ddeb8d6e600c99924ce2
wenkangxi <2441878030@qq.com>
2019-09-05 09:39:04 +0800

    classroom

---
+++ 1806101076温康爔JAVA/src/day20190905/Classroom.java
@@ -0,0 +1,19 @@
+package day20190905;
+
+public class Classroom {
+    //1.属性
+    //长、宽、高……
+    String name;
+    int leight;
+    int weight;
+    int hight;
+
+    //2.使用情况
+    //使用、空置……
+    void use(){
+
+    };
+    void leisure(){
+
+    }
+}
