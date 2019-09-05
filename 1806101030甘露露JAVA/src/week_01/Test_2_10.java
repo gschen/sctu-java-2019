3f58e15e33a61a112940e31be68f453e2aa43635
ganlulu <291500103@qq.com>
2019-09-05 10:59:50 +0800

    我的实验一代码

---
+++ 1806101030甘露露JAVA/src/week_01/Test_2_10.java
@@ -0,0 +1,14 @@
+package week_01;
+
+public class Test_2_10 {
+    public static void main(String[] args) {
+        int row = 1;
+        int col = 1;
+        for(row=1;row<9;row++){
+            for(col=1;col<=row;col++){
+                System.out.print(row+"*"+col+"="+row*col+" " );
+            }
+            System.out.println("");
+        }
+    }
+}
