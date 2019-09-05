a2b375d9a54d482f3a0b40947058412aa8d10401
hyunmun <2366420848@qq.com>
2019-09-05 22:21:38 +0800

    实验2及周末作业

--- 1806101074赵贤雯JAVA/src/week_1/Test_2_10.java
+++ 1806101074赵贤雯JAVA/src/week_1/Test_2_10.java
@@ -1,4 +1,16 @@
 package week_1;
-
+//打印乘法表
 public class Test_2_10 {
+    public static void main(String[] args) {
+        int row = 0;
+        int col = 0;
+
+        for (row = 1; row <= 9; row++){
+
+            for (col = 1; col <= row; col++){
+                System.out.print(col+"*"+ row+"="+row*col+ " ");
+            }
+            System.out.println();
+        }
+    }
 }
