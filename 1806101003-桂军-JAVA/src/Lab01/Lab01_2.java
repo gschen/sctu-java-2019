9892a0ca1edda5739d35febe1545a0085b2024e9
uneventful <878452073@qq.com>
2019-09-05 11:07:10 +0800

    实验一

---
+++ 1806101003-桂军-JAVA/src/Lab01/Lab01_2.java
@@ -0,0 +1,14 @@
+package Lab01;
+
+public class Lab01_2 {
+    public static void main(String[] args) {
+        System.out.println("这是100-300之间的不能被3整除的数：");
+        for(int i=100;i<=300;i++){
+            if(i%3==0){
+                continue;
+            }
+            System.out.printf("%d ",i);
+        }
+        System.out.println();
+    }
+}
