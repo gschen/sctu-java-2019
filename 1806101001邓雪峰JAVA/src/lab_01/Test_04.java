967224e1cb4603113070a7af4e4d9b07284da901
DengSnowMountain <2576468332>
2019-09-09 19:51:01 +0800

    实验1-3

---
+++ 1806101001邓雪峰JAVA/src/lab_01/Test_04.java
@@ -0,0 +1,29 @@
+package lab_01;
+
+public class Test_04 {
+    public static void main(String args[]){
+        //for循环
+        int sum = 0;
+        for (int i = 1; i <=100 ; i++) {
+            sum+=i;
+        }
+        System.out.println(sum);
+
+        int sum1 = 0;
+        int j = 1;
+        while (j<=100 ){
+            sum1+=j;
+            j++;
+        }
+        System.out.println(sum1);
+
+        int sum2 = 0;
+        int n = 1;
+        do {
+            sum2+=n;
+            n++;
+        }while (n<=100);
+
+        System.out.println(sum2);
+    }
+}
---
+++ 1806101001邓雪峰JAVA/src/lab_01/Test_05.java
@@ -0,0 +1,27 @@
+package lab_01;
+
+public class Test_05 {
+    public static void main(String args[]){
+        double ran = Math.random();
+        int i = (int)(ran * 5);
+        switch (i){
+            case 1:
+                System.out.println("不及格");
+                break;
+            case 2:
+                System.out.println("及格");
+                break;
+            case 3:
+                System.out.println("中等");
+                break;
+            case 4:
+                System.out.println("良好");
+                break;
+            case 5:
+                System.out.println("优秀");
+                break;
+        }
+
+
+    }
+}
