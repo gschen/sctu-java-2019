eaa0749e33bac4dd7c54bfc90b72f59cb9fee246
HS123456 <2485026774@qq.com>
2019-09-10 16:00:13 +0800

    实验二

---
+++ 1806101007侯帅JAVA/src/lab02/Test_01.java
@@ -0,0 +1,27 @@
+package lab02;
+
+public class Test_01 {
+    String country;
+    String province;
+    String city;
+    String street;
+    String post;
+
+    void tell(){
+        System.out.println(country+"\n"+province+"\n"+city+"\n"+street+"\n"+post);
+    }
+}
+class Getinformation{
+    public static void main(String[] args) {
+
+        Test_01 add = new Test_01();
+        add.country = "china";
+        add.province = "sichuan";
+        add.city = "yibing";
+        add.street = "null";
+        add.post = "644000";
+        add.tell();
+
+    }
+}
+
