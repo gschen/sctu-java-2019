a48154dbf1f311269ac0074d07417a545c27902a
xiaofwnghuang <hxf08520215>
2019-09-12 11:42:53 +0800

    实验2与课堂练习

---
+++ 1806101025黄晓锋JAVA/src/day20190912/Student.java
@@ -0,0 +1,15 @@
+package day20190912;
+
+public class Student {
+    String xh;
+    String xm;
+
+    Student(String xh,String xm){
+        this.xh = xh;
+        this.xm = xm;
+        System.out.println("学号是：" + xh + "   " + "姓名是：" + xm);
+    }
+
+
+
+}
