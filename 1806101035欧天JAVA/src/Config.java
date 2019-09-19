1fd6fcbea8f137d7f72d2da819ab9a39e37b9a07
wxjot <467108798@qq.com>
2019-09-19 09:47:27 +0800

    为中华之崛起而读书

---
+++ 1806101035欧天JAVA/src/Config.java
@@ -0,0 +1,14 @@
+public class Config {
+    static String URL="www.sctu.edu.cn";
+    static int PORT = 8080;
+
+    static void printInfo() {
+        System.out.println("b");
+    }
+    static String printInfo="asd";
+    static void sky(){
+        System.out.println("a");
+    }
+
+
+}
