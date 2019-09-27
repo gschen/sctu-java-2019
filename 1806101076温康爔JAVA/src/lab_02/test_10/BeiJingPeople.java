5861aee0bafa5bb6dd0f34ba6d16c66c9a7382df
wenkangxi <2441878030@qq.com>
2019-09-27 16:48:58 +0800

    test10

---
+++ 1806101076温康爔JAVA/src/lab_02/test_10/BeiJingPeople.java
@@ -0,0 +1,20 @@
+package lab_02.test_10;
+
+public class BeiJingPeople {
+    protected String weight,height,opera;
+    public  void speakHello(){
+        System.out.println("您好！");
+    }
+    public void averageHeight(){
+        height="175cm";
+        System.out.println("average height:"+height);
+    }
+    public  void averageWeight(){
+        weight="70kg";
+        System.out.println("average weight:"+weight);
+    }
+    public void beijingOpera(){
+        opera="京剧";
+        System.out.println("北京戏曲："+opera);
+    }
+}
---
+++ 1806101076温康爔JAVA/src/lab_02/test_10/People.java
@@ -0,0 +1,16 @@
+package lab_02.test_10;
+
+public class People {
+    protected String weight,height;
+    public  void speakHello(){
+        System.out.println("hello");
+    }
+    public void averageHeight(){
+        height="173cm";
+        System.out.println("average height:"+height);
+    }
+    public  void averageWeight(){
+        weight="70kg";
+        System.out.println("average weight:"+weight);
+    }
+}
