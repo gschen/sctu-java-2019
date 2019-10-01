fca86f074c5e56a598394a4c1783e0a89b67f5d1
wenkangxi <2441878030@qq.com>
2019-10-01 18:54:47 +0800

    test16

---
+++ 1806101076温康爔JAVA/src/lab_02/test_16/Main.java
@@ -0,0 +1,11 @@
+package lab_02.test_16;
+
+public class Main {
+    public static void main(String[] args) {
+        add h1=new add();
+        sub h2=new sub();
+        mul h3=new mul();
+        div h4=new div();
+        System.out.println("1+2="+h1.A(1,2)+" 1-2="+h2.A(1, 2)+" 1*2="+h3.A(1, 2)+"1/2="+h4.A(1, 2));
+    }
+}
