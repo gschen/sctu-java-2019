c04c5dcd8b1da2c9825c58b60b5d2d20ff3c74bd
guanxueqi <1529322913@qq.com>
2019-10-05 17:32:08 +0800

    给定一个字符串，将大写转换为小写，数字转化为0，其他字符转化为#

---
+++ 1806101055官学琦JAVA/src/lab_02/Test_22.java
@@ -0,0 +1,22 @@
+package lab_02;
+
+public class Test_22 {
+    public static void main(String[] args) {
+        String str="Hello1231254Wold()[]MY1son";
+        char[] chars=str.toCharArray();
+        for (int i = 0; i <chars.length ; i++) {
+            if (chars[i]>='A' && chars[i]<='Z'){
+                chars[i]+=32;
+            }
+            if (chars[i]>=48&&chars[i]<=57||chars[i]>65&&chars[i]<=90||chars[i]>=97&&chars[i]<=122 ){
+            }
+            else {
+                chars[i]='#';
+            }
+        }
+        String strs=String.valueOf(chars);
+        String s=strs.replaceAll("\\d","0");
+        System.out.println(s);
+
+    }
+}
