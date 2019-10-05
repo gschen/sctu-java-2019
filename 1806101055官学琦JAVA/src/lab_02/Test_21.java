7271459e015f76e9f6f30cc147aaf20f346dfe6c
guanxueqi <1529322913@qq.com>
2019-10-05 17:08:22 +0800

    输入一个字符串，判断字符串中某一单词出现的次数

---
+++ 1806101055官学琦JAVA/src/lab_02/Test_21.java
@@ -0,0 +1,17 @@
+package lab_02;
+
+import java.util.Scanner;
+
+public class Test_21 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("请输入字符串：");
+        String str=input.next();
+        int str_num=str.length();
+        System.out.println("请输入需要统计的字符串：");
+        String s=input.next();
+        String strs=str.replaceAll(s,"");
+        int strs_num=strs.length();
+        System.out.println(s+"出现的次数为："+(str_num-strs_num)/s.length()+"次");
+    }
+}
