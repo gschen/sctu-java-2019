d93eb6e90f5976a1832147c05d2d2c5c623f77b8
lijuan <2890005237@qq.com>
2019-10-08 21:03:05 +0800

    练习

---
+++ 1806101040李娟JAVA/src/lei1/number11math/NumMix.java
@@ -0,0 +1,11 @@
+package lei1.number11math;
+
+public class NumMix {
+    public static void main(String args[]){
+        System.out.println(Math.max(12.123, 18.456));
+        System.out.println(Math.max(23.12, 23.0));
+        System.out.println(Math.min(12,11));
+        System.out.println(Math.min(13.4,15.9));
+    }
+
+}
