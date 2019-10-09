21ed982b4e01ba77a4bc53768c488074af3ab3f9
pony <1176632450@qq.com>
2019-10-09 20:34:43 +0800

    作业整理

--- 1806101035欧天JAVA/src/day20190905/test_6/Ladder.java
+++ 1806101035欧天JAVA/src/day20190905/test_6/Ladder.java
@@ -1,4 +1,4 @@
-package day20190905.test_6;
+package lab_02.day20190905.test_6;
 
 public class Ladder {
 }
--- 1806101049马原涛JAVA/src/day20190919/test_8/USB.java
+++
@@ -1,6 +0,0 @@
-package day20190919.test_8;
-
-public interface USB {
-    public void start();
-    public void stop();
-}
