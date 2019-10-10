86bd1d4bdf80eee0c8868f75f6b5ac3e7d5e9138
liuyujiangx <1914571065@qq.com>
2019-10-10 21:18:19 +0800

    实验二

---
+++ 1806101073刘玉江JAVA/src/week_1/Test01.java
@@ -0,0 +1,36 @@
+package week_1;
+
+import java.io.*;
+
+public class Test01 {
+    public static void main(String[] args) {
+
+        BufferedReader reader = null;
+        try {
+             reader = new BufferedReader(
+                    new FileReader("D:\\demo.docx")
+            );
+            String line = null;
+            while (
+                    (line = reader.readLine()) != null
+                    ){
+
+                System.out.println(line);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            //关闭文件
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            
+        }
+    }
+}
