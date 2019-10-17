4edb5e0c5db4dae2532f69bd73ab8d6eff07b949
guanxueqi <1529322913@qq.com>
2019-10-17 09:08:05 +0800

    Test_03

--- 1806101055官学琦JAVA/src/lab_03/Test_03/Test_03.java
+++ 1806101055官学琦JAVA/src/lab_03/Test_03/Test_03.java
@@ -6,7 +6,6 @@ public class Test_03 {
     public static void main(String[] args) throws IOException {
         BufferedWriter writer=new BufferedWriter(new FileWriter("C:\\Users\\官学琦\\Desktop\\Java实验报告\\1.txt"));
         writer.write("Beijing欢迎你！");
-        writer.close();
         System.out.println("完成");
         BufferedReader reader=new BufferedReader(new FileReader("C:\\Users\\官学琦\\Desktop\\Java实验报告\\1.txt"));
         String line=null;
@@ -15,9 +14,8 @@ public class Test_03 {
         }
         reader.close();
         System.out.println("完成");
-        BufferedWriter writer1=new BufferedWriter(new FileWriter("C:\\Users\\官学琦\\Desktop\\Java实验报告\\1.txt"));
-        writer1.append("\n我的学号是：你自己的学号");
-        writer1.close();
+        writer.append("\n我的学号是：你自己的学号");
+        writer.close();
         System.out.println("完成");
 
     }
